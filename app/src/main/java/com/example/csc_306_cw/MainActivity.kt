1beb686eb5fbb460789fc023b21e4a74d755712f
Fahd17 <fahdsattam8@gmail.com>
2023-03-04 00:00:30 +0000

    Google map is working

--- app/src/main/java/com/example/csc_306_cw/MainActivity.kt
+++ app/src/main/java/com/example/csc_306_cw/MainActivity.kt
@@ -5,13 +5,25 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), OnMapReadyCallback{
+
+     lateinit var map: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.artefactsmenu)
+
+        var mapFragment: SupportMapFragment  = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         var atrefacts = popilateList()
         var atrefact = atrefacts.get(1)
@@ -95,5 +107,14 @@ class MainActivity : AppCompatActivity() {
         modalities.add(R.drawable.next5)
         return modalities
 
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        map = googleMap
+        var campus: LatLng = LatLng(51.619296, -3.878914)
+        map.addMarker(MarkerOptions().position(campus).title("Computational Foundry"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(campus))
     }
 }
